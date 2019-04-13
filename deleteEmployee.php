b14365c8bdc570bd8fd7273b2fe1c53d58a4f0e6
crt5qz <crt5qz@virginia.edu>
2019-04-13 19:11:08 -0400

    added the employee stuff!

---
+++ deleteEmployee.php
@@ -0,0 +1,20 @@
+<?php
+ include_once("./library.php"); // To connect to the database
+ $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " .
+mysqli_connect_error();
+ }
+ // Form the SQL query (a DELETE query)
+ $sql="DELETE FROM Employee WHERE rest_id = '$_POST[rest_id]' AND emp_id = '$_POST[emp_id]'";
+
+ if (!mysqli_query($con,$sql))
+ {
+ echo $USERNAME;
+ die('Error: ' . mysqli_error($con));
+ }
+ echo "1 record added"; // Output to user
+ mysqli_close($con);
+?>
